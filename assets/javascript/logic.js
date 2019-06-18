d210e685cf6a2dfc9627cc4c6091510caa8fa496
Steve Archibald <stevearchi21@gmail.com>
2019-06-18 12:08:13 -0600

    merging with Charlies code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 var amenitiesArray = [];
 
-$(".custom-control-input").on("click", function() {
+$(".custom-control-input").on("click", function () {
   //console.log($(this).attr("data-check"));
   if ($(this).attr("data-check") === "false") {
     $(this).attr("data-check", "true");
@@ -9,7 +9,7 @@ $(".custom-control-input").on("click", function() {
   }
 });
 
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
   event.preventDefault();
   var amenitiesArray = [];
   if ($("#showers").attr("data-check") === "true") {
@@ -38,70 +38,73 @@ $("#submit").on("click", function(event) {
   campQueryParams.q = $("#location")
     .val()
     .trim();
-//   campQueryParams.fields = JSON.stringify(amenitiesArray);
+  //   campQueryParams.fields = JSON.stringify(amenitiesArray);
 
- // console.log(campQueryParams);
-// can't get the amenities array to function properly in the URL, but since they don't really functionally change the search results i think that's ok
-  
-// var parkName = $("#location").val().trim()
+  // console.log(campQueryParams);
+  // can't get the amenities array to function properly in the URL, but since they don't really functionally change the search results i think that's ok
 
-// var parkUrl = "https://developer.nps.gov/api/v1/parks?api_key=IvDm5VJctJF8OHMsxVyrHXjVShQNgrTwYSbzQrYJ&q=" + parkName
+  // var parkName = $("#location").val().trim()
 
-// $.ajax({
-//   url: parkUrl,
-//   method: "GET",
-// }).then(function (response) {
-//   var parkResults = response.data;
-//   var parkCode = parkResults.parkCode
-//   campQueryParams.parkCode = parkCode;
-//   console.log("campQueryParams: ", campQueryParams)
-// })
+  // var parkUrl = "https://developer.nps.gov/api/v1/parks?api_key=IvDm5VJctJF8OHMsxVyrHXjVShQNgrTwYSbzQrYJ&q=" + parkName
+
+  // $.ajax({
+  //   url: parkUrl,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   var parkResults = response.data;
+  //   var parkCode = parkResults.parkCode
+  //   campQueryParams.parkCode = parkCode;
+  //   console.log("campQueryParams: ", campQueryParams)
+  // })
 
 
- 
-var campgroundUrl =
+
+  var campgroundUrl =
     "https://developer.nps.gov/api/v1/campgrounds?" + $.param(campQueryParams);
 
-    $.ajax({
-      url: campgroundUrl,
-      method: "GET"
-    }).then(function(response) {
-      var campResults = response.data;
-      console.log(campResults)
+  $.ajax({
+    url: campgroundUrl,
+    method: "GET"
+  }).then(function (response) {
+    var campResults = response.data;
+    console.log(campResults)
 
-    })
+  })
 });
 
 
 var darkskyKey = "24ad87e96d744bd3fb31284ccc8763a1"
 var weatherUrl = "https://api.darksky.net/forecast/" + darkskyKey + "/[latitude],[longitude],[time]"
 var campQueryParams = {
-    "api_key": "IvDm5VJctJF8OHMsxVyrHXjVShQNgrTwYSbzQrYJ",
+  "api_key": "IvDm5VJctJF8OHMsxVyrHXjVShQNgrTwYSbzQrYJ",
 }
 
 function getWeather(lat, long, date) {
-  latLongString =lat + "," + long + "," + date;
+  latLongString = lat + "," + long + "," + date;
   $.ajax({
-      method: 'GET',
-      url: weatherUrl
-  }).then(function (response){
-  })
+    method: 'GET',
+    url: weatherUrl
+  }).then(function (response) {
 
-  $('#submit').on('click', function (event) {
-      event.preventDefault();
-      startDate = $('#start-date').val().trim();
-      startMoment = moment(startDate, 'MM/DD/YYYY');
-      endDate = $('#end-date').val().trim();
-  
-      $.ajax({
-          method: 'GET',
-          url: campgroundUrl
-      }).then(function (response) {
-          //loop through each campground
-        // JSON.object
-        // parkLatLong
-          getWeather(latLong.lat, latLong.long, startDate);
-      });
-  
-      // clear out values
-      //$('#location').val('');
+  });
+}
+
+$('#submit').on('click', function (event) {
+  event.preventDefault();
+  var latLong = {
+    lat: 0,
+    long: 0
+  }
+  startDate = $('#start-date').val().trim();
+  startMoment = moment(startDate, 'MM/DD/YYYY');
+  endDate = $('#end-date').val().trim();
+
+
+    JSON.parse(parkLatLong);
+    console.log(parkLatLong);
+    getWeather(latLong.lat, latLong.long, startDate);
+
+
+  // clear out values
+  //$('#location').val('');
+})
